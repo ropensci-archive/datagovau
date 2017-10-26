181694b913ecb7ce031af4b50443bb4eefcde92a
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-10-26 14:03:38 +1100

    annotations to Jono's examples on which ones which work and which ones don't.

--- scribble/jono-original-usage-examples.R
+++ scribble/jono-original-usage-examples.R
@@ -2,29 +2,50 @@
 # Some of these take a long time to run!
 
 
-#### USAGE:
-
 library(datagovau)
 library(dplyr)
+library(mapview)
+
+#----------------water-----------
+# doesn't work ' unexpected unzipping of files'
 res <- search_data("name:water", limit = 20)
 res %>% filter(can_use == "yes") %>% slice(2) %>% show_data
 
+
+
+#-------------fire---------------
+# downloads data but pars;es columns rwrong
 res <- search_data("name:fire", limit = 20)
 res %>% filter(can_use == "yes") %>% slice(3) %>% show_data %>% View
-res %>% filter(can_use == "yes") %>% slice(4) %>% show_data
 
-# res <- search_data("name:population", limit = 20)
-# res %>% filter(can_use == "yes") %>% View
-# res %>% filter(can_use == "yes") %>% slice(5) %>% show_data() %>% View
+# works:
+res %>% filter(can_use == "yes") %>% slice(4) %>% show_data %>% View
 
-# res <- search_data("name:location", limit = 100)
-# res %>% filter(can_use == "yes", format == "SHP") %>% View
-# res %>% filter(can_use == "yes", format == "SHP") %>% slice(6) %>% show_data()
-# res %>% filter(can_use == "yes", format == "SHP") %>% slice(7) %>% show_data()
 
+#--------------population---------------
+res <- search_data("name:population", limit = 20)
+
+# works iff library(mapview) loaded first...
+res %>% filter(can_use == "yes") %>% slice(5) %>% show_data() 
+
+
+#-----------------location-------------
+res <- search_data("name:location", limit = 100)
+res %>% filter(can_use == "yes", format == "SHP") %>% View
+
+res %>% filter(can_use == "yes", format == "SHP") %>% slice(6) %>% show_data()
+
+# sorry can't work with this file yet (because only 6 rows of res have "yes" in can_use)
+res %>% filter(can_use == "yes", format == "SHP") %>% slice(7) %>% show_data()
+
+#------------------format shapefile--------------------
 res <- search_data("format:SHP", limit = 100)
 res %>% filter(can_use == "yes") %>% View
+
+# works:
 res %>% filter(can_use == "yes") %>% slice(4) %>% show_data()
+
+# dowloads ok then takes 5+ minutes to render on screen so beware.
 res %>% filter(can_use == "yes") %>% slice(22) %>% show_data()
 
 # res <- search_data("description:QLD", limit = 100) ## BIG!
