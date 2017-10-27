275830492f7e8fc7fe2851b51757e7b69d9256f8
HughParsonage <hugh.parsonage@gmail.com>
2017-10-27 13:52:45 +1100

    Import `data.table`, but `dplyr` (not used)
    import <package> is generally frowned upon, but is ok for `data.table`, unless there's a namespace collision (which roxygen2 warn about).

---
+++ pkg/R/datagovau-package.R
@@ -0,0 +1,11 @@
+#' Retrieve data from data.gov.au
+#' @name package-data.gov.au
+#' @docType package
+#' @keywords package
+#' @description Provides an interface for R users to search and load data
+#' from the Australian Government's data portal, <https://data.gov.au>.
+#'
+#'
+#' @import data.table
+#'
+"_PACKAGE"
